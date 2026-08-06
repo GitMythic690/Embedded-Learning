37c5dd2313124d392083338c3bce22ee6981884e
Michael Isaacs <m.isaacs690@gmail.com>
2026-08-06 10:07:54 +0100

    Complete Mario double pyramid - nested loop

--- nestedloop.c
+++ nestedloop.c
@@ -32,27 +32,3 @@ int main(void)
     }
     
 }
-/*
-int main(void) {
-    printf("Enter number of rows\n");
-    scanf("%d", &height);
-    int FIXED_SPACES = 2;
-
-    for (int i = 1; i < height; i++) {
-        //printf("%d\n",i);
-        int spaces_available = height - i;
-        int needed_hashtags = i * 2;
-        
-        //printf("Spaces Available: %d\n", spaces_available);
-        //printf("Needed Hashtag: %d\n", needed_hashtags);
-        //printf("%d\n", spaces_available + needed_hashtags);
-
-        int total_characters = spaces_available + FIXED_SPACES + needed_hashtags;
-        for (int i = 1; i < total_characters; i++) {
-
-            // output spaces_available, then needed_hashtags/2 then FIXED_SPACES then needed_hashtags/2
-            
-        }
-    }
-}*/
-
